8e7c1d420350a4957068a18396d2ff8c3d0bcd45
Shivam Singh <shivamvns8840@gmail.com>
2025-11-06 21:25:27 +0530

    updation in code

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -7,7 +7,7 @@ const ChatContainer = ({selectedUser,setSelectedUser}) => {
       <div className='flex items-center gap-3 py-4 mx-4 border-b border-stone-500'>
         <img src={assets.profile_martin} alt="" className='w-8 rounded-full' />
         <p  className='flex-1 text-lg text-white   items-center gap-2'>
-          Martin Johnson
+          Marten Johnson
           <span className='w-2 h-2 rounded-full  bg-green-499'></span>
         </p>
         <img onClick={()=>{
